54e056133f038aeefa0598f3f54f99bf69f069c1
Barış Günenç <power.of.baris@gmail.com>
2022-05-31 10:19:13 +0300

    layout imigration

---
+++ PuniPuniBookWeb/Areas/Customer/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PuniPuniBook.Web.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
