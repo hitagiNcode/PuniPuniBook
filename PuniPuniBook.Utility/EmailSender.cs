dd463323ac0c6c32c6e1d78705aba07519a2356c
Barış Günenç <power.of.baris@gmail.com>
2022-06-03 17:11:53 +0300

    next is searchbar func

--- PuniPuniBook.Utility/EmailSender.cs
+++ PuniPuniBook.Utility/EmailSender.cs
@@ -23,25 +23,8 @@ namespace PuniPuniBook.Utility
 
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            //var emailToSend = new MimeMessage();
-            //emailToSend.From.Add(MailboxAddress.Parse("hello@dotnetmastery.com"));
-            //emailToSend.To.Add(MailboxAddress.Parse(email));
-            //emailToSend.Subject = subject;
-            //emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html){ Text = htmlMessage};
-
-            ////send email
-            //using (var emailClient = new SmtpClient())
-            //{
-            //    emailClient.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
-            //    emailClient.Authenticate("dotnetmastery@gmail.com", "DotNet123$");
-            //    emailClient.Send(emailToSend);
-            //    emailClient.Disconnect(true);
-            //}
-
-            //return Task.CompletedTask;
-
             var client = new SendGridClient(SendGridSecret);
-            var from = new EmailAddress("hello@dotnetmastery.com", "Bulky Book");
+            var from = new EmailAddress("info@punipunibook.com", "PuniPuni Book");
             var to = new EmailAddress(email);
             var msg = MailHelper.CreateSingleEmail(from, to, subject,"", htmlMessage);
             return client.SendEmailAsync(msg);
